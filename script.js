b97f8d53839c7f3d5228a6e489d992007be27378
Dani-Diac <77856443+Dani-Diac@users.noreply.github.com>
2021-04-09 17:41:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-var correct_btn = Math.round(Math.random() * 2) + 1 ;
+var correct_btn = null;
+
+
+function create_buttons() {
+  var input = document.getElementById('getInputValue').value;
+  if (input < 1 || input > 100) {
+    alert("Please write a number between 1-100!")
+    return false;
+  }
+  correct_btn = Math.floor(Math.random() * input);
+  for (let i = 0; i < input; i++) {
+    $('#buttons').append('<button type="button"  id='+ i +' onclick="return check_btn(this.id)"> Click me! </button>');
+  }
+  document.getElementById("getInputValue").style.display="none";
+  document.getElementById("createBtns").style.display="none";
+  return fals
+}
+
+
 function check_btn(id) {
   if (id == correct_btn) {
-    alert("You're guess it's right. Try again!");
+    alert("You're guess it's right!");
+    location.reload();
   } else {
     alert("You're wrong!")
     return false;
